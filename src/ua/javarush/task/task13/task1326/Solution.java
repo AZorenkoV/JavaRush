ccaf3a04bac9067ac521712dcd924e0db84bde59
AZorenko <azorenko@gmail.com>
2025-05-16 12:37:59 +0300

    [Work] Task 1526

--- src/ua/javarush/task/task13/task1326/Solution.java
+++ src/ua/javarush/task/task13/task1326/Solution.java
@@ -1,17 +1,7 @@
 package ua.javarush.task.task13.task1326;
 
-import java.io.BufferedReader;
-<<<<<<< HEAD
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
-=======
 import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.*;
->>>>>>> e191078573e612eb98d35cc2fe8e009db272c61c
 
 /* 
 Сортування парних чисел із файлу
@@ -20,9 +10,6 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         // напишіть тут ваш код
-<<<<<<< HEAD
-
-=======
         Scanner consol = new Scanner(System.in);
         try(FileInputStream fis = new FileInputStream(consol.nextLine())){
             //fis.readAllBytes().
@@ -34,6 +21,5 @@ public class Solution {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
->>>>>>> e191078573e612eb98d35cc2fe8e009db272c61c
     }
 }
