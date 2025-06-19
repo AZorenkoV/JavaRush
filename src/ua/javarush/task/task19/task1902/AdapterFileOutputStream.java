fc7e05594bf5794111ffa9a283ff2d34aefc64a2
AZorenkoV <azorenko@gmail.com>
2025-06-19 11:05:03 +0300

    Задачі 1901 та 1902

---
+++ src/ua/javarush/task/task19/task1902/AdapterFileOutputStream.java
@@ -0,0 +1,37 @@
+package ua.javarush.task.task19.task1902;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/* 
+Адаптер
+*/
+
+public class AdapterFileOutputStream implements AmigoStringWriter{
+
+    private FileOutputStream fileOutputStream;
+
+    public static void main(String[] args) {
+
+    }
+
+    public AdapterFileOutputStream(FileOutputStream fis){
+        this.fileOutputStream = fis;
+    }
+
+    @Override
+    public void flush() throws IOException {
+        this.fileOutputStream.flush();
+    }
+
+    @Override
+    public void writeString(String s) throws IOException {
+        this.fileOutputStream.write(s.getBytes());
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.fileOutputStream.close();
+    }
+}
+
