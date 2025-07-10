b2c04666400f57e5cc8ae43c4b9c17b9f0a792d8
AZorenkoV <azorenko@gmail.com>
2025-07-10 11:19:34 +0300

    Задача 2024

---
+++ src/ua/javarush/task/task20/task2024/Solution.java
@@ -0,0 +1,18 @@
+package ua.javarush.task.task20.task2024;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Знайомство з графами
+*/
+
+public class Solution implements Serializable {
+    int node;
+    List<Solution> edges = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+    }
+}
